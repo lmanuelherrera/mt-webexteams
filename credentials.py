f53fd496beb7fdf56e1841423f499847e074c5fa
lmanuelherrera <lmanuelherrera@unicesar.edu.co>
2023-10-02 10:42:30 -0500

    pruebas

--- credentials.py
+++ credentials.py
@@ -1,14 +1,14 @@
 #Set to '' if you don-t want to send alerts to Webex
-WT_ACCESS_TOKEN=''
-WT_ROOM_ID=''
+WT_ACCESS_TOKEN='NWNlM2NjZjYtMjhjZS00ZDdiLTkyNTMtZmQ1ZmMxZDM5NzZiNmE2N2U2OWQtODY3_PF84_b4e50a79-b7de-4ee2-940a-a983d1e5c35b'
+WT_ROOM_ID='Y2lzY29zcGFyazovL3VzL1JPT00vYTc1YTgzOTAtNTYzMC0xMWVlLTk2MTEtMDU3YWFkYjZhNWM3'
 #Mandatory
-MERAKI_API_KEY=''
-MERAKI_ORG_ID=''
-MERAKI_NET_ID=''
-MERAKI_CAMS=['']
+MERAKI_API_KEY='8f7fae459f872d4a64493ec0df5d28c2152d68e3'
+MERAKI_ORG_ID='1042395'
+MERAKI_NET_ID='L_779685685488518317'
+MERAKI_CAMS=['Q2GV-HY57-H937','Q2HV-FU4B-5CRT','Q2PV-AUAR-4K3G','Q2PV-TNTC-G8TZ','Q2PV-YHGR-9HGY','Q2PV-64NS-7G4F','Q2PV-C7SJ-ERD7','Q2PV-ZEE6-RRXG']
 MERAKI_BASEURL='https://api.meraki.com/api/v1'
 #Set to '' if you don't want to send alerts to MS Teams or Slack
-MS_TEAMS_URL=''
+MS_TEAMS_URL='https://intellego365.webhook.office.com/webhookb2/51ee0df2-da6a-4546-a7c6-c4dd1996316b@00a05ce0-bd3d-4215-a569-c6261a20a39e/IncomingWebhook/32f21017b58d4e7c9cebd78867af2e2c/6d7d8f86-a22c-4098-8447-dc58315e185b'
 SLACK_URL=''
 #Set to 0 if you don't want time based alerts
 #Configure a value in seconds to wait after a door is opened
